df137b4eb76c164912a716037045ce75c4673bef
YangJinMo <didwlsah9@gmail.com>
2021-04-13 16:31:56 +0900

    Add Comment

--- RxSwift/MVVMRxSwift/MVVMRxSwift/ViewController.swift
+++ RxSwift/MVVMRxSwift/MVVMRxSwift/ViewController.swift
@@ -12,17 +12,31 @@ import RxCocoa
 class ViewController: UIViewController {
   
   let disposeBag: DisposeBag = DisposeBag()
+  
+  /*
+   declare our view model
+   there's a number of ways of doing this
+   
+   however we know that this view controller needs this RestaurantsListModel
+   essentially to operate so we're going to force unwrap it
+   
+   because it can't work without this view model
+   and if we force unwrap it, it means every time we use it.
+   
+   it needs to have a value otherwise the album crash
+   */
   private var viewModel: RestaurantsListModel!
   
   @IBOutlet weak var tableView: UITableView!
   
+  // so the perfect place to do this just to pass it into our instantiate method here
   static func instantiate(viewModel: RestaurantsListModel) -> ViewController {
     let storyboard = UIStoryboard(name: "Main", bundle: .main)
     let viewController = storyboard.instantiateInitialViewController() as! ViewController
-    viewController.viewModel = viewModel
+    viewController.viewModel = viewModel // just pass it in there we can set it on the view controller
     return viewController
   }
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -32,6 +46,11 @@ class ViewController: UIViewController {
     navigationController?.navigationBar.prefersLargeTitles = true
     tableView.contentInsetAdjustmentBehavior = .never
     
+    /*
+     and that means when our view loads we can actually subscribe to our fetch restaurant view model
+     but before we can subscribe to this function we need finish it off in the view model
+     so come back to your restaurants list view model
+     */
     viewModel
       .fetchRestaurantViewModels()
       .observe(on: MainScheduler.instance)
